d2e77e2b9cf05c92d38fc9846db065bf18e71a17
Will Badart <badart_william@bah.com>
2020-01-27 14:28:26 -0500

    Removed Frame example from Main
    Simplified Frames module. Also added data file to the app directory

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies #-}
 
 module Main where
 
@@ -20,19 +18,8 @@ data Career = Career
   }
   deriving Show
 
-data PrestigeFrame = PrestigeFrame
-  { unk       :: Vector Text
-  , education :: Vector Double
-  , income    :: Vector Int
-  , women     :: Vector Double
-  , prestige  :: Vector Double
-  , census    :: Vector Int
-  , type_     :: Vector Text
-  }
-  deriving Show
-
 instance Record Career where
-  readLine delim line =
+  parseLine delim line =
     let cells = splitOn (pack delim) (pack line)
     in Career
       (cells !! 0)
@@ -43,17 +30,6 @@ instance Record Career where
       (read $ (unpack $ cells !! 5))
       (cells !! 6)
 
-instance Frame PrestigeFrame Career where
-  parseCSV delim = map (readLine delim) . lines
-  rowsToFrame records = PrestigeFrame
-    (fromList $ map unkR records)
-    (fromList $ map educationR records)
-    (fromList $ map incomeR records)
-    (fromList $ map womenR records)
-    (fromList $ map prestigeR records)
-    (fromList $ map censusR records)
-    (fromList $ map type_R records)
-
 main = do
-  df <- readCSV @Career @PrestigeFrame "," "/Users/williambadart/Downloads/Prestige2.csv"
+  df <- readCSV @Career "," "./app/prestige.csv"
   print df
--- src/Hdsk/Frames.hs
+++ src/Hdsk/Frames.hs
@@ -10,26 +10,33 @@ Portability : POSIX
 TODO
 -}
 
-{-# LANGUAGE AllowAmbiguousTypes   #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeApplications      #-}
-{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE DataKinds #-}
 
 module Hdsk.Frames
--- (
--- ) where
-where
+( Record(..)
+, readCSV
+) where
 
-import Data.Kind
-import Language.Haskell.TH hiding (Type)
+import Hdsk.Preprocessing (PreprocessedBy, noPreprocess)
 
+-- | A 'Record' type represents an individual data object in the target domain.
+-- It's interface is a sort of simplified 'Read', which parses one line of text
+-- into a 'Record'.
 class Record r where
-  readLine :: String -> String -> r
+  -- | Parse a line (or any string) into an @r@.
+  parseLine
+    :: String  -- ^ Arbitrary string parameter, typically @delim@
+    -> String  -- ^ The text to parse
+    -> r
 
-class Record record => Frame frame record where
-  rowsToFrame :: [record] -> frame
-  parseCSV :: String -> String -> [record]
+-- | Read a list of 'Record's from tabluar data on disk, and record that it is
+-- raw data (i.e. no preprocessing has yet been done).
+readCSV
+  :: Record r
+  => String    -- ^ Delimiter
+  -> FilePath  -- ^ Path to data
+  -> IO (PreprocessedBy '[] [r])
+readCSV delim path = noPreprocess <$> readCSV' delim path
 
-readCSV :: forall r f. (Record r, Frame f r) => String -> FilePath -> IO f
-readCSV delim path = rowsToFrame @f @r . parseCSV @f delim <$> readFile path
+readCSV' :: Record r => String -> FilePath -> IO [r]
+readCSV' delim path = map (parseLine delim) . lines <$> readFile path
