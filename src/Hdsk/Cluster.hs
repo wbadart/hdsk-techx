b1844fed385253d60f13083aae30345c3e4b3f89
Will Badart <badart_william@bah.com>
2020-01-27 15:14:04 -0500

    Cluster: example of more descriptive error message
    Gives links to further reading and documentation.

--- src/Hdsk/Cluster.hs
+++ src/Hdsk/Cluster.hs
@@ -14,6 +14,8 @@ Here we implement a number of different clustering algorithms.
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
@@ -23,20 +25,22 @@ module Hdsk.Cluster
 ) where
 
 import GHC.TypeLits (ErrorMessage(..))
-import Hdsk.Preprocessing (Normalizer, PreprocessedBy)
+
 import Hdsk.Internal.Types (Member)
+import Hdsk.Preprocessing (Normalizer, PreprocessedBy)
 
 -- | Use DBSCAN to compute a clustering of the pre-normalized dataset.
 dbscan :: Member algs Normalizer (
-       Text "DBSCAN works best with normalized data."
-  :$$: Text "Please apply a Normalizer to your data before moving on."
+       'Text "DBSCAN works best with normalized data."
+  ':$$: 'Text "Please apply a Normalizer to your data before moving on."
   ) => PreprocessedBy algs (f a) -> f clusterId
 dbscan = undefined
 
--- | Calculate the clustering of the pre-normalized dataset using KMeans.
--- TODO: check assumption of convex clusters, as well as choice of /K/?
-kmeans :: Member algs Normalizer (
-       Text "KMeans doesn't give meaningful results on unnormalized data."
-  :$$: Text "Please apply a Normalizer to your data first."
-  ) => PreprocessedBy algs (f a) -> f clusterId
+kmeans
+  :: Member algs Normalizer
+          ( Text "Clustering only gives meaningful results for normalized data. "
+       :$$: Text "    See https://bit.ly/2FUHTai for further explanation."
+       :$$: Text "Please apply a normalizer to your data before clustering. "
+       :$$: Text "    See https://TODO for a list of normalizers." )
+  => Int -> PreprocessedBy algs df -> [cluster]
 kmeans = undefined
